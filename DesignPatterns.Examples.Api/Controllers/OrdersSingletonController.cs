a9aabbe0cdc3a365b62ff6ef5d45791ca349ceee
Jefferson Veiga <gelsvsampa@hotmail.com>
2025-10-03 17:35:44 -0300

    Singleton

---
+++ DesignPatterns.Examples.Api/Controllers/OrdersSingletonController.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns.Examples.Application.Models;
+using DesignPatterns.Examples.Infrastructure.Creational.Singleton;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Examples.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersSingletonController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post(
+    OrderInputModel model
+    )
+    {
+        return Ok(BusinessHours.GetInstance());
+    }
+}
---
+++ DesignPatterns.Examples.Infrastructure/Creational/Singleton/BusinessHours.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns.Examples.Infrastructure.Creational.Singleton;
+
+public class BusinessHours
+{
+    private BusinessHours(DateTime startTime, DateTime endTime)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+
+    private static BusinessHours? _instance;
+
+    public DateTime StartTime { get; private set; }
+    public DateTime EndTime { get; private set; }
+
+    public static BusinessHours GetInstance()
+    {
+        if (_instance is null)
+        {
+            _instance = new BusinessHours(
+                new DateTime(1, 1, 1, new Random().Next(0, 10), 0, 0),
+                new DateTime(1, 1, 1, new Random().Next(18, 24), 0, 0)
+            );
+
+            Console.WriteLine("Nova instância gerada");
+        }
+
+        return _instance;
+    }
+}
