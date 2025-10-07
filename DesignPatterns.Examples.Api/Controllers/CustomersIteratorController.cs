a70901a5cc311865bb183948148aabf90d413cc9
Jefferson Veiga <gelsvsampa@hotmail.com>
2025-10-06 22:18:21 -0300

    Iterator

---
+++ DesignPatterns.Examples.Api/Controllers/CustomersIteratorController.cs
@@ -0,0 +1,27 @@
+﻿using DesignPatterns.Examples.Infrastructure.Behavioral.Iterator;
+using DesignPatterns.Examples.Infrastructure.Customers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Examples.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersIteratorController : ControllerBase
+{
+    [HttpGet("report-notify-blocked-customers")]
+    public IActionResult NotifyBlockedCustomerEmail([FromServices] ICustomerRepository repository)
+    {
+        List<Core.Entities.Customer> blockedCustomers = repository.GetBlockedCustomers();
+
+        CustomersToNotifyQueryModel query = new(blockedCustomers, "LuisDev");
+
+        foreach (KeyValuePair<string, string> customer in query)
+        {
+            Console.WriteLine($"Customer: {customer.Key}, Email: {customer.Value}");
+        }
+
+        Console.WriteLine($"Utilizando acesso direto: {query["Fulano 1"]}");
+
+        return Ok();
+    }
+}
